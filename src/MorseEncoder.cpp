5ffeb30dbfd8493b28e5071a463096eb9c292219
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-09-12 13:12:39 +0530

    PANNIC UPDATEEE AAAAAAAAAAAAAAAHH

--- src/MorseEncoder.cpp
+++ src/MorseEncoder.cpp
@@ -29,67 +29,72 @@ void MorseEncoder::encodeLight(String text) {
 }
 
 void MorseEncoder::encode(String text, int mode) {
-  int len = text.length();
-  for (int i = 0; i < len; i++) {
-    char oneLetter = toupper(text.charAt(i)); // convert to uppercase directly.
-    switch (oneLetter) {
-      case 'A': A(mode); break;
-      case 'B': B(mode); break;
-      case 'C': C(mode); break;
-      case 'D': D(mode); break;
-      case 'E': E(mode); break;
-      case 'F': LF(mode); break;
-      case 'G': G(mode); break;
-      case 'H': H(mode); break;
-      case 'I': I(mode); break;
-      case 'J': J(mode); break;
-      case 'K': K(mode); break;
-      case 'L': L(mode); break;
-      case 'M': M(mode); break;
-      case 'N': N(mode); break;
-      case 'O': O(mode); break;
-      case 'P': P(mode); break;
-      case 'Q': Q(mode); break;
-      case 'R': R(mode); break;
-      case 'S': S(mode); break;
-      case 'T': T(mode); break;
-      case 'U': U(mode); break;
-      case 'V': V(mode); break;
-      case 'W': W(mode); break;
-      case 'X': X(mode); break;
-      case 'Y': Y(mode); break;
-      case 'Z': Z(mode); break;
-      case ' ': space(); break;
-      case '0': N0(mode); break;
-      case '1': N1(mode); break;
-      case '2': N2(mode); break;
-      case '3': N3(mode); break;
-      case '4': N4(mode); break;
-      case '5': N5(mode); break;
-      case '6': N6(mode); break;
-      case '7': N7(mode); break;
-      case '8': N8(mode); break;
-      case '9': N9(mode); break;
-      case '.': SfullStop(mode); break;
-      case ':': Scolon(mode); break;
-      case ',': Scomma(mode); break;
-      case ';': Ssemicolon(mode); break;
-      case '?': Squestion(mode); break;
-      case '=': Sequal(mode); break;
-      case '\'': Ssquotation(mode); break;
-      case '/': Sslash(mode); break;
-      case '!': Sexclamation(mode); break;
-      case '-': Shyphen(mode); break;
-      case '_': Sunderscore(mode); break;
-      case '"': Sdquotation(mode); break;
-      case '(': Slbracket(mode); break;
-      case ')': Srbracket(mode); break;
-      case '$': Sdollar(mode); break;
-      case '&': Sampersand(mode); break;
-      case '@': Sat(mode); break;
-      case '+': Splus(mode); break;
-      default: // unrecognized characters or symbols here.
-        break;
+  text.toUpperCase();
+  for (int i = 0; i < text.length(); i++) {
+    char character = text.charAt(i);
+    //char character = 'O'; // Replace with the letter you want to print
+    int index = character - 'A'; // Calculate the index for the letter
+
+    if (index >= 0 && index < 26) {
+      const char* morse = reinterpret_cast<const char*>(pgm_read_word(&morseCodes[index])); // Get the Morse code for the letter
+
+      for (int j = 0; morse[j] != '\0'; j++) {
+        if (morse[j] == '.') {
+          dot(mode);
+        } else if (morse[j] == '-') {
+          dash(mode);
+        }
+        //Serial.print(morse[j]); // Print each individual character of Morse code
+      }
+
+      //Serial.println(); // Print a new line to separate Morse codes
+      letterSpace();
+    } else if (character >= '0' && character <= '9') {
+      // Handle numbers if needed
+      int index = character - '0' + 26; // Calculate the index for the digit
+
+      const char* morse = reinterpret_cast<const char*>(pgm_read_word(&morseCodes[index])); // Get the Morse code for the digit
+
+      for (int j = 0; morse[j] != '\0'; j++) {
+        if (morse[j] == '.') {
+          dot(mode);
+        } else if (morse[j] == '-') {
+          dash(mode);
+        }
+        //Serial.print(morse[j]);
+      }
+      //Serial.println();
+      letterSpace();
+    } else if (character == ' ') {
+      // Handle space if needed
+      space();
+    } else {
+      // Handle other characters or invalid characters as needed
+      int index = -1;
+
+      // Check if the character is one of the special characters
+      const char specialChars[] = ".,:;?=/!-_\"()$@&+";
+      for (int j = 0; j < sizeof(specialChars) - 1; j++) {
+        if (character == specialChars[j]) {
+          index = j;
+          break;
+        }
+      }
+
+      if (index >= 0 && index < 17) {
+        const char* morse = reinterpret_cast<const char*>(pgm_read_word(&morseSpecialChars[index])); // Get the Morse code for the character
+
+        for (int j = 0; morse[j] != '\0'; j++) {
+          if (morse[j] == '.') {
+            dot(mode);
+          } else if (morse[j] == '-') {
+            dash(mode);
+          }
+          //Serial.print(morse[j]);
+        }
+        //Serial.println();
+        letterSpace();
+      }
     }
   }
 }
