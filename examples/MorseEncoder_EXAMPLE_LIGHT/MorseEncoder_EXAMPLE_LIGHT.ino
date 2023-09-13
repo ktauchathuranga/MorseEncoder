e97f35af37ce522cbb91efe7c8bdd5436adcdbcc
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-09-14 01:45:46 +0530

    Update MorseEncoder_EXAMPLE_LIGHT.ino

--- examples/MorseEncoder_EXAMPLE_LIGHT/MorseEncoder_EXAMPLE_LIGHT.ino
+++ examples/MorseEncoder_EXAMPLE_LIGHT/MorseEncoder_EXAMPLE_LIGHT.ino
@@ -1,6 +1,6 @@
 #include "MorseEncoder.h"
 
-MorseEncoder morseL(13); // Initialize with the desired pin for Light
+MorseEncoder morseL(13);      // Initialize with the desired pin for Light
 
 void setup() {
   morseL.beginLight(15);      // [LIGHT] Initialize WPM (word per minute) rate
@@ -8,8 +8,8 @@ void setup() {
 
 void loop() {
   String message = "sos";
-  morseA.print(message); // [LIGHT] Boradcast encoded light >> or you can use morseA.encodeAudio(message); **for the strings only
+  morseL.print(message);   // [LIGHT] Boradcast encoded light >> or you can use morseL.encodeLight(message); **for the strings only
   delay(2000);
-  morseA.print(123);     // [LIGHT] its inherits from Print class, so you can encode various types of data
+  morseL.print(123);       // [LIGHT] its inherits from Print class, so you can encode various types of data
   delay(2000);
 }
