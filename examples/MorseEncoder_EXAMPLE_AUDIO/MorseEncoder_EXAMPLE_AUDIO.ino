0ffec91802882bdbea88ecf26c6c5654cd841926
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-09-14 01:41:09 +0530

    Update MorseEncoder_EXAMPLE_AUDIO.ino

--- examples/MorseEncoder_EXAMPLE_AUDIO/MorseEncoder_EXAMPLE_AUDIO.ino
+++ examples/MorseEncoder_EXAMPLE_AUDIO/MorseEncoder_EXAMPLE_AUDIO.ino
@@ -1,6 +1,6 @@
 #include "MorseEncoder.h"
 
-MorseEncoder morseA(9);  // Initialize with the desired pin for Audio
+MorseEncoder morseA(9);  // Initialize with the desired pin for Audio **PWM pins only
 
 void setup() {
   morseA.beginAudio(15, 500); // [AUDIO] Initialize WPM (word per minute rate) rate and frequency
