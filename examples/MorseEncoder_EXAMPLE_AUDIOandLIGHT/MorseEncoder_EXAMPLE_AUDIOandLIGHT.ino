487fff2457253e478cf8ca08574828117560d88b
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-09-14 01:43:01 +0530

    Update MorseEncoder_EXAMPLE_AUDIOandLIGHT.ino

--- examples/MorseEncoder_EXAMPLE_AUDIOandLIGHT/MorseEncoder_EXAMPLE_AUDIOandLIGHT.ino
+++ examples/MorseEncoder_EXAMPLE_AUDIOandLIGHT/MorseEncoder_EXAMPLE_AUDIOandLIGHT.ino
@@ -1,6 +1,6 @@
 #include "MorseEncoder.h"
 
-MorseEncoder morseA(9);  // Initialize with the desired pin for Audio
+MorseEncoder morseA(9);  // Initialize with the desired pin for Audio **PWM pins only
 MorseEncoder morseL(13); // Initialize with the desired pin for Light
 
 void setup() {
@@ -10,8 +10,8 @@ void setup() {
 
 void loop() {
   String message = "sos";
-  morseA.encodeAudio(message); // [AUDIO] Boradcast encoded audio
+  morseA.print(message); // [AUDIO] Boradcast encoded audio >> or you can use morseA.encodeAudio(message); **for the strings only
   delay(2000);
-  morseL.encodeLight(message); // [LIGHT] Broadcast encoded light
+  morseL.print(message); // [LIGHT] Broadcast encoded light
   delay(2000);
 }
