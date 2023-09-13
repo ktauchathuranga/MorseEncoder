c001a089c84f34b32052d26d3c9b755162d27c72
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-09-14 01:48:16 +0530

    Update MorseEncoder_EXAMPLE_AUDIO.ino

--- examples/MorseEncoder_EXAMPLE_AUDIO/MorseEncoder_EXAMPLE_AUDIO.ino
+++ examples/MorseEncoder_EXAMPLE_AUDIO/MorseEncoder_EXAMPLE_AUDIO.ino
@@ -1,6 +1,6 @@
 #include "MorseEncoder.h"
 
-MorseEncoder morseA(9);  // Initialize with the desired pin for Audio **PWM pins only
+MorseEncoder morseA(9);       // Initialize with the desired pin for Audio **PWM pins only
 
 void setup() {
   morseA.beginAudio(15, 500); // [AUDIO] Initialize WPM (word per minute rate) rate and frequency
@@ -8,8 +8,8 @@ void setup() {
 
 void loop() {
   String message = "sos";
-  morseA.print(message); // [AUDIO] Boradcast encoded audio >> or you can use morseA.encodeAudio(message); **for the strings only
+  morseA.print(message);   // [AUDIO] Boradcast encoded audio >> or you can use morseA.encodeAudio(message); **for the strings only
   delay(2000);
-  morseA.print(123);     // [AUDIO] its inherits from Print class, so you can encode various types of data
+  morseA.print(123);       // [AUDIO] its inherits from Print class, so you can encode various types of data
   delay(2000);
 }
